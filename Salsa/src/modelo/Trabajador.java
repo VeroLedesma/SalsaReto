dc8ba356eddefcf1d6984e18134b6b06b98d3726
Vero <vledesmalavega@gmail.com>
2024-05-02 13:13:10 +0200

    Añadir las clases

---
+++ Salsa/src/modelo/Trabajador.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public class Trabajador extends Persona {
+
+		public String nnss;
+
+		public String getNnss() {
+			return nnss;
+		}
+
+		public void setNnss(String nnss) {
+			this.nnss = nnss;
+		}
+
+
+}
